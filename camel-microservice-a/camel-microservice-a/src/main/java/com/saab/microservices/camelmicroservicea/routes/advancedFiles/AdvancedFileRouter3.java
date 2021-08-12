4ca45f430ebe44c4f862ea7c11d0b04717d59a9a
its-Saab <musaababs@gmail.com>
2021-08-12 11:05:56 +0200

    Comment out @component

--- camel-microservice-a/camel-microservice-a/src/main/java/com/saab/microservices/camelmicroservicea/routes/advancedFiles/AdvancedFileRouter3.java
+++ camel-microservice-a/camel-microservice-a/src/main/java/com/saab/microservices/camelmicroservicea/routes/advancedFiles/AdvancedFileRouter3.java
@@ -4,7 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class AdvancedFileRouter3 extends RouteBuilder {
 
     @Autowired
